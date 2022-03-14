68b2247f9cba13ef24d7ff2f4e6fb8f2ee5320f8
Patrick Bowen <patbowen168@gmail.com>
2022-03-13 21:09:23 -0300

    pbtest7

--- spotluck/blog/models.py
+++ spotluck/blog/models.py
@@ -14,41 +14,42 @@ class Post(models.Model):
 
 
 class Users(models.Model):
-    u_id = models.primary_key()
+    u_id = models.IntegerField()
     name = models.CharField(max_length=100)
     location_address = models.TextField()
     phone_number = models.IntegerField()
     email_address = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
-    b_id = models.IntegerField(unique=true)
-    s_id = models.IntegerField(unique=true)
+    b_id = models.IntegerField()
+    s_id = models.IntegerField()
 
     def __str__(self):
         return self.u_id
 
 class Orders(models.Model):
-    o_id = models.primary_key()
-    b_id = models.ForeignKey(Users)
-    s_id = models.ForeignKey(Users)
-    PURCHASED_CHOICES = [(YES, 'Yes'), (NO, 'No')]
-    purchased = models.CharField(choices=PURCHASED_CHOICES, default=NO)
+    o_id = models.IntegerField()
+    #b_id = models.ForeignKey(Users, on_delete=models.DO_NOTHING)
+    #s_id = models.ForeignKey(Users, on_delete=models.DO_NOTHING)
+    #PURCHASED_CHOICES = [(YES, 'Yes'), (NO, 'No')]
+    #purchased = models.CharField(choices=PURCHASED_CHOICES, default=NO) ########################################
+    purchased = models.CharField(max_length=100)
 
     def __str__(self):
         return self.o_id
 
+class Items(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    unit_price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
 class Includes(models.Model):
-    includes_id = models.primary_key()
-    o_id = models.ForeignKey(Orders)
-    item_id = models.ForeignKey(Items)
+    includes_id = models.IntegerField()
+    o_id = models.ForeignKey(Orders, on_delete=models.DO_NOTHING)
+    item_id = models.ForeignKey(Items, on_delete=models.DO_NOTHING)
     quantity = models.IntegerField()
 
     def __str__(self):
         return self.includes_id
-
-class Items(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    unit_price = models.DecimalField(max_digits=5, max_digits=2)
-
-    def __str__(self):
-        return self.name
